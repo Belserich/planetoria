24b967db0fcf98aa7eb80d6fb295bca217d430e8
Maximilian Bels <belserich@DeusExMachina.knechtnet.io>
2019-04-10 22:14:00 +0200

    Initial commit

---
+++ desktop/src/com/github/belserich/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.belserich.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.belserich.GameClient;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameClient(), config);
+	}
+}
