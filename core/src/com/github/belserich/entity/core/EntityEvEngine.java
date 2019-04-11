5fde4dfa2b8321bacd4e6579a85dd3808872d142
Belserich <maximilianbels@gmail.com>
2019-04-11 22:38:31 +0200

    Rename base entity engine for clarification and document code

--- core/src/com/github/belserich/entity/core/EntityEvEngine.java
+++ core/src/com/github/belserich/entity/core/EntityEvEngine.java
@@ -3,16 +3,22 @@ package com.github.belserich.entity.core;
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.EntitySystem;
 
-public class BaseEngine extends Engine {
+/**
+ * An entity event engine is a standard engine with an additional dispose method.
+ */
+public class EntityEvEngine extends Engine {
 	
+	/**
+	 * Removes all systems and disposes them.
+	 */
 	public void dispose() {
 		
 		this.removeAllEntities();
 		
 		for (EntitySystem sys : this.getSystems()) {
 			this.removeSystem(sys);
-			if (sys instanceof EntityFlagSystem) {
-				((EntityFlagSystem) sys).dispose();
+			if (sys instanceof EntityEvSystem) {
+				((EntityEvSystem) sys).dispose();
 			}
 		}
 	}
