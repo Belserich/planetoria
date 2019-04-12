068f4282b6f5dcaf0e221fc6250e4c93902bd8e7
Belserich <maximilianbels@gmail.com>
2019-04-12 17:57:28 +0200

    Create life component (card lifepoints)

---
+++ core/src/com/github/belserich/entity/component/LifeComponent.java
@@ -0,0 +1,12 @@
+package com.github.belserich.entity.component;
+
+import com.badlogic.ashley.core.Component;
+
+public class LifeComponent implements Component {
+	
+	public float pts;
+	
+	public LifeComponent(float pts) {
+		this.pts = pts;
+	}
+}
