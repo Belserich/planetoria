5695abb0f9660d21a12e42249e7037915bc807f3
Belserich <maximilianbels@gmail.com>
2019-04-12 17:59:12 +0200

    Create shield component (card shield points)

---
+++ core/src/com/github/belserich/entity/component/ShieldComponent.java
@@ -0,0 +1,12 @@
+package com.github.belserich.entity.component;
+
+import com.badlogic.ashley.core.Component;
+
+public class ShieldComponent implements Component {
+	
+	public float pts;
+	
+	public ShieldComponent(float pts) {
+		this.pts = pts;
+	}
+}
