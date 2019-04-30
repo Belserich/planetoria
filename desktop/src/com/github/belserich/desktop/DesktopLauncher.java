9f4874119e695334c6b12f50efcfb89e060a19ee
Belserich <maximilianbels@gmail.com>
2019-04-30 23:30:06 +0200

    Increase launcher size

--- desktop/src/com/github/belserich/desktop/DesktopLauncher.java
+++ desktop/src/com/github/belserich/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import com.github.belserich.GameClient;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 800;
-		config.height = 600;
+		config.width = 1200;
+		config.height = 900;
 		new LwjglApplication(new GameClient(), config);
 	}
 }
