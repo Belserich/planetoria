628865a5da12fff905d3813eade5e598f38cb71f
Belserich <maximilianbels@gmail.com>
2019-05-04 14:49:41 +0200

    Create base shield-related events

---
+++ core/src/com/github/belserich/entity/event/attack/CardAttackNoSpEvent.java
@@ -0,0 +1,11 @@
+package com.github.belserich.entity.event.attack;
+
+import com.badlogic.ashley.core.Entity;
+import com.github.belserich.entity.event.base.CardAttackBaseEvent;
+
+public class CardAttackNoSpEvent extends CardAttackBaseEvent {
+	
+	public CardAttackNoSpEvent(Entity primary, Entity destCard, float attackPts, Entity... others) {
+		super(primary, destCard, attackPts, others);
+	}
+}
---
+++ core/src/com/github/belserich/entity/event/attack/CardDestroySpEvent.java
@@ -0,0 +1,18 @@
+package com.github.belserich.entity.event.attack;
+
+import com.badlogic.ashley.core.Entity;
+import com.github.belserich.entity.event.base.CardAttackBaseEvent;
+
+public class CardDestroySpEvent extends CardAttackBaseEvent {
+	
+	private final float shieldPoints;
+	
+	public CardDestroySpEvent(Entity sourceCard, Entity destCard, float attackPts, float shieldPoints, Entity... others) {
+		super(sourceCard, destCard, attackPts, others);
+		this.shieldPoints = shieldPoints;
+	}
+	
+	public float shieldPoints() {
+		return shieldPoints;
+	}
+}
