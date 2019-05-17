f81185b36dcdab649a2deb1b7bb495f6f35f0114
Belserich <maximilianbels@gmail.com>
2019-05-17 18:36:26 +0200

    Add sub system for reflecting card zone-changes in the ui

--- core/src/com/github/belserich/entity/system/UiSystem.java
+++ core/src/com/github/belserich/entity/system/UiSystem.java
@@ -1,21 +1,50 @@
 package com.github.belserich.entity.system;
 
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.github.belserich.entity.component.Lp;
 import com.github.belserich.entity.component.Sp;
 import com.github.belserich.entity.component.Ui;
+import com.github.belserich.entity.component.ZoneParent;
 import com.github.belserich.entity.core.BaseEntitySystem;
+import com.github.belserich.entity.core.Mappers;
+import com.github.belserich.ui.GameUi;
+import com.github.belserich.ui.ZoneUi;
 
 public class UiSystem extends BaseEntitySystem {
 	
-	public UiSystem() {
+	private GameUi gameUi;
+	private ZoneParentChangeSystem zoneParentChangeSys;
+	
+	public UiSystem(GameUi gameUi) {
 		super(Family.all(
 				Ui.Card.class
 		).one(
 				Sp.Broke.class,
 				Lp.Changed.class
 		).get(), 10);
+		this.gameUi = gameUi;
+		
+		zoneParentChangeSys = new ZoneParentChangeSystem();
+	}
+	
+	@Override
+	public void addedToEngine(Engine engine) {
+		super.addedToEngine(engine);
+		engine.addSystem(zoneParentChangeSys);
+	}
+	
+	@Override
+	public void removedFromEngine(Engine engine) {
+		super.removedFromEngine(engine);
+		engine.removeSystem(zoneParentChangeSys);
+	}
+	
+	@Override
+	public void update(float delta) {
+		zoneParentChangeSys.update(delta);
+		super.update(delta);
 	}
 	
 	@Override
@@ -33,5 +62,33 @@ public class UiSystem extends BaseEntitySystem {
 		if (lc != null) {
 			uic.ui.setLp(String.valueOf(lc.pts));
 		} else uic.ui.setLp(String.valueOf(0f));
+	}
+	
+	private class ZoneParentChangeSystem extends BaseEntitySystem {
+		
+		public ZoneParentChangeSystem() {
+			super(Family.all(
+					Ui.Card.class,
+					ZoneParent.Changed.class
+			).get());
+		}
+		
+		@Override
+		public void update(Entity entity) {
+			
+			Ui.Card uic = Mappers.uiCard.get(entity);
+			ZoneParent.Changed zpc = Mappers.zoneParentChanged.get(entity);
+			
+			ZoneUi lastUi = gameUi.getZoneUi(zpc.last);
+			ZoneUi nowUi = gameUi.getZoneUi(zpc.now);
+			
+			if (lastUi != null) {
+				lastUi.removeCardUi(uic.ui);
+			}
+			
+			if (nowUi != null) {
+				nowUi.addCardUi(uic.ui);
+			}
+		}
 	}
 }
