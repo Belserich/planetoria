645e6c8a5ca0060bf2127a743d00b6e89fad6001
Belserich <maximilianbels@gmail.com>
2019-06-18 08:47:23 +0200

    Remove obsolete mappers class

--- core/src/com/github/belserich/entity/core/Mappers.java
+++
@@ -1,9 +0,0 @@
-package com.github.belserich.entity.core;
-
-import com.badlogic.ashley.core.ComponentMapper;
-import com.github.belserich.entity.component.Zone;
-
-public class Mappers {
-	
-	public static final ComponentMapper<Zone> zoneParent = ComponentMapper.getFor(Zone.class);
-}
