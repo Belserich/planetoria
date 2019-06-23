24b9f49a2d9e6a7562ede85c36c1db2184502e00
Belserich <maximilianbels@gmail.com>
2019-06-23 21:45:58 +0200

    Create occupiable component to indicate occupiable fields

---
+++ core/src/com/github/belserich/entity/component/Occupiable.java
@@ -0,0 +1,9 @@
+package com.github.belserich.entity.component;
+
+import com.badlogic.ashley.core.Component;
+
+public class Occupiable implements Component {
+	
+	public static class Just implements Component {
+	}
+}
